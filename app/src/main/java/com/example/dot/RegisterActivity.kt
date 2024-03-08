41b7eecdd0cc5a04d08103db3e553c23e39ca6df
choyunju <yoonj1860@naver.com>
2024-03-08 15:03:22 +0900

    design : register 레이아웃 구현

--- app/src/main/java/com/example/dot/RegisterActivity.kt
+++ app/src/main/java/com/example/dot/RegisterActivity.kt
@@ -1,11 +1,35 @@
 package com.example.dot
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //뒤로가기 버튼(register -> login)
+        val back = findViewById<ImageView>(R.id.back)
+        back.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        //회원가입 완료 버튼 클릭 시 로그인 화면 이동
+        val btn_register = findViewById<Button>(R.id.btn_register)
+        btn_register.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
+
+
 }
